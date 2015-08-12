a56083f55012dc696cd38c7a05380adb44bef5b2
nasang <sktkddusrj@naver.com>
2015-08-13 01:31:25 +0900

    test code

--- tcp.h
+++ tcp.h
@@ -1,6 +1,6 @@
-#define	MY_ID			16	 //my id for addr
+#define	MY_ID			0	 //my id for addr
 
-#define	SERV_TCP_PORT	(7000 + MY_ID) // tcp port num
+#define	SERV_TCP_PORT	(9000 + MY_ID) // tcp port num
 #define	SERV_HOST_ADDR	"112.108.39.255" // host IP
 
 #define	MSG_REQUEST		1 //request msg
